a0e6c892199e5d7bad6e3438d8116e4bfbebed1c
LieC4 <danielmorillascarpio@gmail.com>
2022-07-12 18:08:57 +0200

    Iteration#2

--- app.js
+++ app.js
@@ -6,3 +6,15 @@ let x = 50;
 let h = 5;
 let y = 10;
 let z = x + 5;
+
+// iteracion 2 .
+
+const character = { name: 'Jack Sparrow', age: 10 };
+console.log(character);
+character.age = 25;
+console.log(character);
+
+
+let firstName = "Jon";
+let lastName = "Snow";
+let age = 24;
