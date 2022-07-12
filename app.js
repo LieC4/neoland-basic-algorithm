b8084187607df15fbd8e37c00e967fdf271bd1c7
LieC4 <danielmorillascarpio@gmail.com>
2022-07-12 18:23:44 +0200

    Iteration#5

--- app.js
+++ app.js
@@ -113,3 +113,32 @@ console.log("LA DE CONTAR", rickAndMortyCharacters[4]);
 // position - 0, 1, 2
 // lenght 3 lenght - 1 =  2 position
 console.log("LA BUENA", rickAndMortyCharacters[rickAndMortyCharacters.length - 1]);
+
+
+ // Iteracion 5
+
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+// ejemplo
+if (number1 === 10) {
+  console.log('number1 es estrictamente igual a 10')
+}
+
+if ((number2/2) = 2) {
+  console.log("number2 dividido entre number1 es igual a 2");
+}
+
+if (number1 !== number2) {
+  console.log("number1 es estrictamente distinto a number2");
+}
+
+if (number3 != number1) {
+  console.log("number3 es distinto number1");
+}
+
+if ((number3*5) = number1) {
+  console.log("number3 por 5 es igual a number1");
+}
