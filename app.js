98035d7c47c22721ac770bcbd0a03703e43fa87a
LieC4 <danielmorillascarpio@gmail.com>
2022-07-12 18:13:14 +0200

    Iteration#2

--- app.js
+++ app.js
@@ -18,3 +18,35 @@ console.log(character);
 let firstName = "Jon";
 let lastName = "Snow";
 let age = 24;
+
+// 'Soy Jon Snow, tengo 24 años y me gustan los lobos.'
+console.log("soy " + firstName + " " + lastName + ",tengo " + age + " años y me gustan los lobos");
+
+console.log(`soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`);
+
+
+const toy1 = { name: 'Buss myYear', price: 19 };
+const toy2 = { name: 'Rallo mcKing', price: 29 };
+
+console.log("Total Price", toy1.price + toy2.price);
+
+let globalBasePrice = 10000;
+const car1 = {
+  name: 'BMW m&m',
+  basePrice: 50000,
+  finalPrice: 60000
+};
+const car2 = {
+  name: 'Chevrolet Corbina',
+  basePrice: 70000,
+  finalPrice: 80000
+};
+console.log(car1);
+console.log(car2);
+
+globalBasePrice = 25000;
+
+car1.finalPrice = car1.basePrice + globalBasePrice;
+console.log("MODIFICADO", car1);
+car2.finalPrice = car2.basePrice + globalBasePrice;
+console.log("MODIFICADO", car2);
